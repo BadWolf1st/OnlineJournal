fe76d445e75209b5390d1c36149909fc01c866e4
WongIngDonyu <wongingdonyu@gmail.com>
2023-04-27 01:52:36 +0300

    add delete by checkbox and fix table Groups

--- src/main/java/com/example/ediary/repositories/GroupRepository.java
+++ src/main/java/com/example/ediary/repositories/GroupRepository.java
@@ -1,13 +1,11 @@
 package com.example.ediary.repositories;
 
-import com.example.ediary.models.Group;
-import com.example.ediary.models.User;
+import com.example.ediary.models.Group1;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface GroupRepository extends JpaRepository<Group, Long> {
-    List<Group> findByName(String name);
+public interface GroupRepository extends JpaRepository<Group1, Long> {
+    List<Group1> findByName(String name);
 
 }
